2fa326881655b5c895ab6ddf5fed02ac15300e04
ncvald <120143600+ncvald@users.noreply.github.com>
2025-10-02 23:27:53 -0400

    Update test_triangle.py

--- test_triangle.py
+++ test_triangle.py
@@ -15,3 +15,9 @@ def test_invalid_negative():
 
 def test_invalid_triangle_inequality():
     assert classify_triangle(1, 2, 3) == "Invalid"
+
+def test_isosceles_right():
+    assert classify_triangle(1, 1, 2 ** 0.5) == "Isosceles Right"
+
+def test_scalene_non_right():
+    assert classify_triangle(2, 3, 4) == "Scalene"
